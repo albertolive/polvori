2c65a222c3ffc0a1cfc8e49ab7fd1020f7d4aab9
albert olive <albertolivecorbella@gmail.com>
2015-09-06 12:36:22 +0200

    first commit

---
+++ app/scripts/controllers/mainController.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name Polvori.controller:MainController
+ * @description
+ * # MainController
+ */
+angular.module('Polvori')
+  .controller('MainController', function($scope) {
+
+    // do something with $scope
+
+  });
---
+++ app/scripts/controllers/settingsController.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name Polvori.controller:SettingsController
+ * @description
+ * # SettingsController
+ */
+angular.module('Polvori')
+  .controller('SettingsController', function($scope) {
+
+    // do something with $scope
+
+  });
